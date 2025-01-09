a00205793029c2c51d05609525cc3a105144e4ff
Anthonybuer182 <1348172474@qq.com>
2025-01-09 20:33:05 +0800

    角色样式修改

--- src/app/_components/character.js
+++ src/app/_components/character.js
@@ -1,24 +1,38 @@
-import { Image, Card, CardFooter,Button,Te } from "@nextui-org/react";
+import { Image, Card, CardFooter, Button } from "@nextui-org/react";
+import { IoPlayCircleOutline } from "react-icons/io5";
 
-export default function Character({character}) {
+export default function Character({ character }) {
+  const playSound = () => {
+    const audio = new Audio(character.soundUrl); 
+    audio.play();
+  };
 
   return (
-    <Card >
-        <Image
-          removeWrapper
-          alt="Card example background"
-          className="w-60 h-80 md:mx-auto"
-          src={character.portrait}
-        />
-        <CardFooter className="absolute bottom-0 border-zinc-100/50 z-10 justify-between">
-          <div>
-            <p className="text-white text-tiny">{character.name}</p>
-            <p className="text-white text-tiny">{character.background}</p>
+    <Card>
+      <Image
+        removeWrapper
+        alt="Card example background"
+        className="w-60 h-80 md:mx-auto object-cover" // 使用 object-cover 保持图片比例并剪裁
+        src={character.portrait}
+      />
+      <CardFooter className="absolute bottom-0 border-zinc-100/50 z-10 justify-between">
+        <div className="flex flex-col w-full">
+          <div className="flex justify-between items-center mb-2">
+            <p className="text-white text-lg font-semibold">{character.name}</p>
+            <IoPlayCircleOutline className="text-white w-7 h-7" onPress={playSound} />
           </div>
-          <Button className="text-tiny" color="primary" radius="full" size="sm">
-            Notify Me
-          </Button>
-        </CardFooter>
-      </Card>
+          <p
+            className="text-white text-tiny overflow-hidden"
+            style={{
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
+            }}
+          >
+            {character.background}
+          </p>
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
